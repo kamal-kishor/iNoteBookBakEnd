763d6cc0c1f7c5f6f5630ff3f6b85837497c9875
Kamal Kishor Joshi <51081539+kamal-kishor@users.noreply.github.com>
2023-04-04 18:35:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ app.use(express.json());
 //Available Route
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/notes", require("./routes/notes"));
-// app.get("/", (req, res) => {
-//   res.send("Hello KAMAL !");
-// });
+app.get("/", (req, res) => {
+  res.send("Hello KAMAL !");
+});
 app.listen(port, () =>
   console.log(`iNoteBook Backend listening on port ${port}`)
 );
